e87511b35e408329e3a84af64e198f5b63cb18a1
Guy Indepurker <guy235@gmail.com>
2021-02-15 18:08:39 +0200

    add state by genre

--- src/cmps/MixList/MixList.jsx
+++ src/cmps/MixList/MixList.jsx
@@ -4,10 +4,11 @@ import React, { Component } from 'react'
 import './MixList.scss'
 import MixPreview from '../MixPreview/MixPreview';
 
-export default function MixList({mixes}){
+export default function MixList({mixes,title}){
 
  return (
      <ul className="mix-list clean-list">
+            {title && <h2>{title}</h2>}
               {mixes.map(mix=>{
                 return( <MixPreview key={mix._id} mix={mix} /> )
             })}
