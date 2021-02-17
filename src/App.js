55aa8f95e7577e4844c99665ca75e41f4e65c271
Guy Indepurker <guy235@gmail.com>
2021-02-17 13:03:32 +0200

    add create mix cmp

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import UserProfile from './pages/UserProfile/UserProfile';
 import {MixDetails} from './pages/MixDetails/MixDetails';
 import SignUp from './pages/SignUp/SignUp';
 import { connect } from 'react-redux';
+import {CreateMix} from './cmps/CreateMix/CreateMix';
 class _App extends Component  {
   render(){
     const {user} = this.props
@@ -27,6 +28,7 @@ class _App extends Component  {
       <Router>
         <AppHeader />
         <Switch>
+          <PrivateRoute path='/app/mix/new' component={CreateMix} />
           <PrivateRoute path='/app/mix/:id' component={MixDetails} />
           <PrivateRoute path='/app/mixes' component={Mixes} />
           <PrivateRoute path='/app' component={MusixApp} />
--- src/cmps/CreateMix/CreateMix.jsx
+++ src/cmps/CreateMix/CreateMix.jsx
@@ -2,12 +2,57 @@
 import React, { Component } from 'react'
 
 import './CreateMix.scss'
-
-class CreateMix extends Component {
-
+import { userService } from '../../services/UserService';
+import { mixService } from '../../services/MixService';
+import LoaderCmp from '../LoaderCmp/LoaderCmp';
+import { connect } from 'react-redux';
+import { addMix } from '../../store/actions/MixAction';
+class _CreateMix extends Component {
+    state = {
+        mix:null
+    }
+    componentDidMount(){
+        const miniUser = userService.getMiniUser()
+        const mix = mixService.getEmptyMix(miniUser)
+        this.setState({mix})
+    }
+    handleChange = ({ target }) => {
+        const field = target.name
+        const value = target.value
+        this.setState(prevState=>({ mix:{...prevState.mix,[field]: value }}))
+    }
+    saveMix = async(ev)=>{
+        ev.preventDefault()
+        const {mix} = this.state
+         await this.props.addMix(mix)
+             this.props.history.push(`/app/mix/${this.props.newMix._id}`)
+    }
     render() {
-
+    const {mix}=this.state
+        if(!mix) return <LoaderCmp></LoaderCmp>
+        return (
+            <section className="create-mix">
+                <h3 className="create-mix-title">Create your own mix!</h3>
+                <form onSubmit={this.saveMix} className="form-mix flex column wrap align-center">
+                    <label htmlFor="mixName">Enter your mix name:</label>
+                    <input id="mixName" type="text" onChange={this.handleChange} name="name" value={mix.name} placeholder="mix name" />
+                    <label htmlFor="mixDescription">Enter your mix description:</label>
+                    <input id="mixDescription" onChange={this.handleChange} type="text" name="description" value={mix.description} placeholder="mix name" />
+                    <button>Create mix</button>
+                </form>
+            </section>
+        )
     }
 }
 
-export default CreateMix
+function mapStateToProps(state) {
+    return {
+        newMix: state.mixReducer.mix,
+    }
+}
+
+const mapDispatchToProps = {
+    addMix,
+}
+
+export const CreateMix = connect(mapStateToProps, mapDispatchToProps)(_CreateMix)
