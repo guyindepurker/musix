deb061a3ed7a79545c014a3b90e04b5a24d386ff
Guy Indepurker <guy235@gmail.com>
2021-03-04 16:35:04 +0200

    change master

--- src/cmps/LoaderCmp/LoaderCmp.jsx
+++
@@ -1,9 +0,0 @@
-
-import './LoaderCmp.scss'
-import React from 'react'
-
-const LoaderCmp = () => {
-    return <div className="loader-container flex center-center"><div className="loader-cmp"></div></div>
-}
-
-export default LoaderCmp
